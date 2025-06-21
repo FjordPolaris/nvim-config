cda15661b79aa76185b07668503b8ce4721155e0
POLARIS <yorushika.lyrah@gmail.com>
2025-06-21 20:08:30 +0700

    Add files via upload

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,12 +6,31 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "nord",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+
+	vim.api.nvim_create_user_command("RunCF", function()
+  local ft = vim.bo.filetype
+  local filename = vim.fn.expand("%")
+
+  local cmd = {
+    python = "python3 " .. filename,
+    lua = "lua " .. filename,
+    c = "gcc " .. filename .. " -o " .. vim.fn.expand("%:r") .. " && ./" .. vim.fn.expand("%:r"),
+    cpp = "g++ " .. filename .. " -o " .. vim.fn.expand("%:r") .. " && ./" .. vim.fn.expand("%:r"),
+  }
+
+  if cmd[ft] then
+    vim.cmd("split | terminal " .. cmd[ft])
+  else
+    print("Unsupported filetype: " .. ft)
+  end
+end, {})
+
 }
 
 -- M.nvdash = { load_on_startup = true }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,21 +8,36 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+      opts = {
+      servers = {
+      pyright = {},
+    },
+  },
+
     config = function()
       require "configs.lspconfig"
     end,
   },
 
+  {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { "black" },
+    },
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", "python",
+   		},
+   	},
+  },
+}
 }
